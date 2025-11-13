5bc813430ee6aea74138f3cb0aabd3223107376c
Blessing Oboh <obohblessing878@gmail.com>
2025-11-13 15:21:15 +0100

    my commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// 25 expressions using various operators
+
+// Variables needed for the expressions
+let a = 10;
+let b = 5;
+let c = '5';
+let d = true;
+let e = false;
+
+// ARITHMETIC OPERATORS
+// 1.  Addition
+console.log(a + b);
+
+// 2. Subtraction
+console.log(a - b);
+
+// 3. Multiplication
+console.log(a * b);
+
+// 4. Division
+console.log(a / b);
+console.log(a / 2);
+
+// 5. Modulus
+console.log(a % 3);
+
+// 6. Exponentiation
+console.log(b ** 2);
+
+// ASSIGNMENT OPERATORS
+// 1. Assignment
+let x = 2;
+
+// 2. Add and assign
+// let addResult = 1;
+a += 4;
+console.log(a);
+
+// 3. Subtract and assign
+let result = 20;
+result -= 5;
+console.log(result);
+
+// 4. Multiply and assign
+let result2 = 2
+result2 *= 3;
+console.log(result2);
+
+// 5. Divide and assign
+let result3 = 10;
+result3 /= 2;
+console.log(result3);
+
+// COMPARISON OPERATORS
+// 1. Equal to
+console.log(b == c);
+
+// 2. Strictly equal to
+console.log(b === c);
+
+// 3. Not equal
+console.log(a != b);
+
+// 4. Strictly not equal
+console.log(b !== c);
+
+// 5. Greater than
+console.log(a > b);
+
+// 6. Less than
+console.log(b < 4);
+
+// 7. Greater than or eual to 
+console.log(a >= 10);
+
+// 8. Less than or equal to 
+console.log(b <= 5);
+
+// LOGICAL OPERATORS
+// 1. Logical AND
+console.log(d && e);
+
+// 2. Logical OR 
+console.log(d || e);
+
+// 3. Logical NOT 
+console.log(!d);
+
+
